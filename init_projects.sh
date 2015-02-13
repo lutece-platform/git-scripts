c87ce73164ad3364e606e596fb36a72869cc82da
Cédric Marneux <cmarneux@users.noreply.github.com>
2015-02-13 17:24:24 +0100

    init_project.sh: define user name and user email for git and clone develop branch and switch to its by default

--- init_projects.sh
+++ init_projects.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Variables
+EMAIL=""
+USERNAME=""
+NAME=""
+
+function getUserInfos() {
+	while [ -z "${NAME}" ]; do
+		read -p "Enter your first name and your last name : " NAME
+	done
+	while [ -z "${USERNAME}" ]; do
+		read -p "Enter your github's username : " USERNAME
+	done
+echo $USERNAME $NAME
+	awkProg='/"email"/ {mail=substr($2, 2, length($2)-3)} /"primary"/ {primary=substr($2, 1, length($2)-1)} /"verified"/ {if ($2 == "true" && primary == "true") print mail}'
+	EMAIL="$(curl -s -u $USERNAME https://api.github.com/user/emails | awk "$awkProg")"
+	if [ -z "${EMAIL}" ]; then
+		echo "You have to validate your primary email in github."
+		exit 2
+	fi
+}
+
 function usage {
 	echo "Usage: `basename $0` [-t git|ssh|https]"
 	echo " -t: clone type; default is https"
@@ -33,6 +54,8 @@ while getopts ":t:" opt; do
 	esac
 done
 
+getUserInfos
+
 awkProg="/\"name\"/ { project=substr(\$2,2,length(\$2)-3) } /\"$urltype\"/ { print project \";\" substr(\$2,2,length(\$2)-3)}"
 projects=(`curl -s https://api.github.com/orgs/lutece-platform/repos?per_page=500 | awk "$awkProg" | grep "^lutece"`)
 
@@ -41,11 +64,12 @@ do
 	project=`echo ${projectandurl} | cut -d ';' -f 1`
 	url=`echo ${projectandurl} | cut -d ';' -f 2`
 	category=`echo ${project} | cut -d '-' -f 2`
+	path="$(pwd -P)"
 	if [[ ${category} == "core" ]]
 	then
-		path="${project}"
+		path="${path}/${project}"
 	else
-		path="plugins/${category}/${project}"
+		path="${path}/plugins/${category}/${project}"
 	fi
 	if [[ -d $path ]]
 	then
@@ -55,6 +79,10 @@ do
 		echo " Cloning component : ${project}"
 		echo "--------------------------------------------------------------------------------"
 		git clone ${url} ${path}
+		git --git-dir="${path}/.git" --work-tree="${path}" checkout -b develop
+		git --git-dir="${path}/.git" --work-tree="${path}" pull origin develop
+		git --git-dir="${path}/.git" --work-tree="${path}" config user.email "${EMAIL}"
+		git --git-dir="${path}/.git" --work-tree="${path}" config user.name "${NAME}"
 		echo " "
 	fi
 done;
