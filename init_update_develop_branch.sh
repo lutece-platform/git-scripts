7dbd27606cf6048db37eb95e01b51f8efcb7dd27
Cédric Marneux <cmarneux@users.noreply.github.com>
2015-02-13 11:14:53 +0100

    Add script that can create develop branch or update it in all repositories cloned

---
+++ init_update_develop_branch.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+DEVELOP="develop2"
+
+function currentBranch() {
+	CURRENTBRANCH=$(git branch -l | grep '*' | cut -d ' ' -f 2)
+}
+
+function changeBranch() {
+	if [ -z "$1" ]; then
+		if [ "${CURRENTBRANCH}" == "master" -o "x${CURRENTBRANCH}" == "x" ]; then
+			git checkout ${DEVELOP} -q
+		else
+			git checkout ${CURRENTBRANCH} -q
+		fi
+	else
+		currentBranch
+		gitOpts=""
+		if [ -z "$(git branch -l | grep $1)" ]; then
+			gitOpts="-b"
+		fi
+		git checkout ${gitOpts} "$1" -q
+	fi
+}
+
+function runIt() {
+	silence="> /dev/null 2>&1"
+	echo -n "${1}... "
+	shift
+	while [ ! -z "$1" ]; do
+		eval "$1 $silence"
+		shift
+	done
+	[[ $? -eq 0 ]] && echo "done." || echo "failed."
+}
+
+function usage {
+	echo "Usage: `basename $0`"
+	echo "Just run !"
+	exit 1
+}
+
+awkProg="/\"name\"/ { print substr(\$2,2,length(\$2)-3) }"
+runIt "Retrieving projects list" "projects=(`curl -s https://api.github.com/orgs/lutece-platform/repos?per_page=1000 | awk "$awkProg" | grep "^lutece"`)"
+
+for project in ${projects[*]} 
+do
+	echo "project : $project"
+	category=`echo ${project} | cut -d '-' -f 2`
+	if [[ ${category} == "core" ]]
+	then
+		path="${project}"
+	else
+		path="plugins/${category}/${project}"
+	fi
+	if [ ! -d "${path}" ]; then
+		echo "Clone this project before !"
+		continue
+	fi
+	pushd "${path}" > /dev/null
+	changeBranch ${DEVELOP}
+	if [ -z "$(git branch -r | grep ${DEVELOP})" ]; then
+		runIt "Creating ${DEVELOP} branch" "git push origin ${DEVELOP}"
+	else
+		runIt "Update ${DEVELOP} branch" "git pull origin ${DEVELOP}"
+	fi
+	changeBranch
+	popd > /dev/null
+done
